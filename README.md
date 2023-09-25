178f42d771251969dcdede697b3b5df3dcf262e6
deanOfWalls <deanofwalls@gmail.com>
2023-09-24 21:24:16 -0400

    Added image encoding/decoding and updated README.md

--- README.md
+++ README.md
@@ -2,40 +2,44 @@
 
 ## Overview
 
-The `EncoderDecoderApp` is a simple Java application designed to encode and decode text strings using Base64 encoding. The primary purpose of this application is to hide simple data strings from computer-illiterate people on a shared PC. While this application does not provide robust security measures, it serves as a basic layer of obfuscation for text data.
+The `EncoderDecoderApp` is a Java application designed to encode and decode text strings and images using Base64 encoding. While primarily intended for obfuscating simple data strings from computer-illiterate users on a shared PC, it now also offers basic image encoding and decoding capabilities.
 
 ## Features
 
 - **Encode Text**: Converts a given text string into its Base64 encoded form.
 - **Decode Text**: Converts a Base64 encoded string back to its original text form.
-- **Save to File**: Saves the encoded text to a file named `encodedText.txt`.
+- **Encode Image**: Converts an image file into its Base64 encoded form and saves it to a file.
+- **Decode Image**: Converts a Base64 encoded image string back to its original image form.
+- **Save to File**: Saves the encoded text to a file named `encodedText.txt` and encoded images to `encodedFiles.txt`.
 - **Read from File**: Although not implemented in the main loop, a method exists for reading text from a file.
 
 ## How to Use
 
 1. Run the application.
-2. You will be prompted to enter a command. You have two options:
+2. You will be prompted to enter a command. You have four options:
     - To encode text, type `encode: <your_text_here>` and press Enter.
     - To decode text, type `decode: <your_encoded_text_here>` and press Enter.
-3. The application will either display the decoded text or save the encoded text to a file, depending on your command.
+    - To encode an image, type `encode_image: <image_filename>` and press Enter.
+    - To decode an image, type `decode_image: <index>` and press Enter, where `<index>` is the position of the encoded image in `encodedFiles.txt`.
+3. Depending on your command, the application will either display the decoded text, save the encoded text or image to a file, or decode and save the image.
 
 ## Code Structure
 
 - `main(String[] args)`: The main method that handles user input and calls appropriate methods based on the command.
 - `encode(String text)`: A static method that takes a text string and returns its Base64 encoded form.
 - `decode(String encodedText)`: A static method that takes a Base64 encoded string and returns its decoded form.
-- `saveToFile(String text, String filePath)`: A static method that saves a given text string to a specified file path.
+- `saveToFile(String text, String filePath)`: A static method that saves a given text string or encoded image to a specified file path.
 - `readFromFile(String filePath)`: A static method that reads text from a specified file path and returns it.
 
 ## Limitations
 
 - The application uses Base64 encoding, which is not a secure method of encryption. It's only meant for basic obfuscation.
-- The encoded text is saved to a file named `encodedText.txt`, which could be easily accessed and decoded.
+- Encoded text and images are saved to easily accessible files (`encodedText.txt` and `encodedFiles.txt`), which could be decoded if accessed.
 
 ## Future Enhancements
 
 - Implement a more secure method of encryption.
-- Add the ability to specify a custom file name for saving the encoded text.
+- Add the ability to specify custom file names for saving encoded data.
 - Implement a user interface for easier interaction.
 
 ## Dependencies
--- src/main/java/com/deanOfWalls/simple_encoder/EncoderDecoderApp.java
+++ src/main/java/com/deanOfWalls/simple_encoder/EncoderDecoderApp.java
@@ -1,88 +1,133 @@
-package com.deanOfWalls.simple_encoder;// Import required Java libraries
+package com.deanOfWalls.simple_encoder;
+
+// Import necessary Java libraries
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.List;
 import java.util.Scanner;
 
-// Define the main class EncoderDecoderApp
 public class EncoderDecoderApp {
-    // Define the main method
-    public static void main(String[] args) {
-        // Create a Scanner object to read user input
-        Scanner scanner = new Scanner(System.in);
-        // Declare variables to hold the command and text
-        String command;
-        String text;
-        // Define the file path where encoded text will be saved
-        String filePath = "encodedText.txt";
 
-        // Start an infinite loop to keep the program running
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input from the console
+        Scanner scanner = new Scanner(System.in);
+        String command;  // Variable to store user command
+        String text;     // Variable to store text input from user
+        String filePath = "encodedText.txt";  // Default file path for encoded text
+
+        // Infinite loop to keep the program running and accepting commands
         while (true) {
-            // Prompt the user to enter a command
-            System.out.print("Enter your command (encode/decode): ");
-            // Read the command from the user and use twim to remove leading and trailing whitespaces
+            // Prompt the user for a command
+            System.out.print("Enter your command (encode/decode/encode_image/decode_image): ");
+            // Read the user's command and trim any leading or trailing whitespace
             command = scanner.nextLine().trim();
 
-            // Check if the command starts with "encode: "
+            // Check if the command is to encode text
             if (command.startsWith("encode: ")) {
-                // Extract the text to be encoded
+                // Extract the text to be encoded from the command
                 text = command.substring(8);
-                // Encode the text
+                // Encode the text using Base64
                 String encodedText = encode(text);
                 // Save the encoded text to a file
                 saveToFile(encodedText, filePath);
-                // Notify the user that the text has been encoded and saved
+                // Notify the user of the successful encoding
                 System.out.println("Text encoded and saved to " + filePath);
-            }
-            // Check if the command starts with "decode: "
+            } 
+            // Check if the command is to decode text
             else if (command.startsWith("decode: ")) {
-                // Extract the text to be decoded
+                // Extract the encoded text from the command
                 text = command.substring(8);
-                // Decode the text
+                // Decode the text from Base64
                 String decodedText = decode(text);
-                // Display the decoded text
+                // Display the decoded text to the user
                 System.out.println("Decoded text: " + decodedText);
-            }
-            // Handle invalid commands
+            } 
+            // Check if the command is to encode an image
+            else if (command.startsWith("encode_image: ")) {
+                // Extract the image filename from the command
+                String imageName = command.substring(13).trim();
+                try {
+                    // Read the image file into a byte array
+                    byte[] imageBytes = Files.readAllBytes(Paths.get(imageName));
+                    // Encode the image bytes using Base64
+                    String encodedImage = Base64.getEncoder().encodeToString(imageBytes);
+                    // Save the encoded image to a file
+                    saveToFile(encodedImage, "encodedFiles.txt");
+                    // Add a blank line to separate encoded images in the file
+                    saveToFile("", "encodedFiles.txt");
+                    // Delete the original image file
+                    Files.delete(Paths.get(imageName));
+                    // Notify the user of the successful encoding
+                    System.out.println("Image encoded and saved to encodedFiles.txt");
+                } catch (IOException e) {
+                    // Handle any exceptions that occur during the encoding process
+                    System.out.println("An error occurred while encoding the image: " + e.getMessage());
+                }
+            } 
+            // Check if the command is to decode an image
+            else if (command.startsWith("decode_image: ")) {
+                // Extract the index of the encoded image from the command
+                int imageIndex = Integer.parseInt(command.substring(13).trim());
+                try {
+                    // Read all lines from the encoded images file
+                    List<String> lines = Files.readAllLines(Paths.get("encodedFiles.txt"));
+                    // Get the encoded image string based on the provided index
+                    String encodedImage = lines.get(imageIndex);
+                    // Decode the image bytes from Base64
+                    byte[] decodedImageBytes = Base64.getDecoder().decode(encodedImage);
+                    // Define the output filename for the decoded image
+                    String outputImageName = "decoded_image_" + imageIndex + ".png";
+                    // Write the decoded image bytes to a new image file
+                    Files.write(Paths.get(outputImageName), decodedImageBytes);
+                    // Notify the user of the successful decoding
+                    System.out.println("Image decoded and saved as " + outputImageName);
+                } catch (IOException e) {
+                    // Handle any exceptions that occur during the decoding process
+                    System.out.println("An error occurred while decoding the image: " + e.getMessage());
+                } catch (IndexOutOfBoundsException e) {
+                    // Handle the case where the provided index is out of bounds
+                    System.out.println("Invalid image index.");
+                }
+            } 
             else {
-                // Notify the user that the command is invalid
-                System.out.println("Invalid command. Please use 'encode: <text>' or 'decode: <text>'");
+                // Handle any other commands that are not recognized
+                System.out.println("Invalid command. Please use 'encode: <text>', 'decode: <text>', 'encode_image: <filename>', or 'decode_image: <index>'");
             }
         }
     }
 
-    // Define a method to encode text using Base64
+    // Method to encode a text string using Base64
     public static String encode(String text) {
         return Base64.getEncoder().encodeToString(text.getBytes());
     }
 
-    // Define a method to decode text using Base64
+    // Method to decode a Base64 encoded string
     public static String decode(String encodedText) {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedText);
         return new String(decodedBytes);
     }
 
-    // Define a method to save text to a file
+    // Method to save a string to a specified file
     public static void saveToFile(String text, String filePath) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {  // Set the second parameter to 'true' for appending
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
             writer.write(text);  // Write the text to the file
-            writer.newLine();  // Add a new line
+            writer.newLine();   // Move to the next line
         } catch (IOException e) {
-            // Handle any IOExceptions that occur
+            // Handle any exceptions that occur during the file writing process
             System.out.println("An error occurred while saving the file: " + e.getMessage());
         }
     }
 
-    // Define a method to read text from a file
+    // Method to read a string from a specified file
     public static String readFromFile(String filePath) {
         try {
-            // Read all bytes from the file and convert them to a string
             return new String(Files.readAllBytes(Paths.get(filePath)));
         } catch (IOException e) {
-            // Handle any IOExceptions that occur
+            // Handle any exceptions that occur during the file reading process
             System.out.println("An error occurred while reading the file: " + e.getMessage());
             return null;
         }
